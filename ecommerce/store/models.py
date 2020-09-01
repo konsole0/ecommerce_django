bae1d4af03eac8289e31fc12f8781fbff6029e90
K0ns0le Zer0 <karizma@tutanota.com>
2020-09-02 01:42:02 +0200

    Add Product model

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+
+# User model is exist origianlly in django users management system
 from django.contrib.auth.models import User
 
 class Customer(models.Model):
@@ -6,6 +8,16 @@ class Customer(models.Model):
     name = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
     
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    name = models.FloatField()
+
+    # for checking shipping option
+    digital = models.BooleanField(default=False, null=True, blank=False)
+   
     def __str__(self):
         return self.name
     
