cd5d5e09b9f1faae466ce4d41adcc82d232074d4
K0ns0le Zer0 <karizma@tutanota.com>
2020-09-02 07:05:57 +0200

    Add image filed to Product model

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -17,6 +17,7 @@ class Product(models.Model):
 
     # for checking shipping option
     digital = models.BooleanField(default=False, null=True, blank=False)
+    image = models.ImageField(null=True, blank=True)
    
     def __str__(self):
         return self.name
