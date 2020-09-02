e64093f5230bf9cdf70ecc302c46865fb42315b2
K0ns0le Zer0 <karizma@tutanota.com>
2020-09-02 07:02:04 +0200

    Render products (views)

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import *
 
 def store(request):
-	context = {}
+	products = Product.objects.all()
+	context = {
+		'products': products
+	}
 	return render(request, 'store/store.html', context)
 
 def cart(request):
