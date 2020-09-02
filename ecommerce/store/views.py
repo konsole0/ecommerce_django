c159981dd90f5d3f5de60b44626b35ce5a51c866
K0ns0le Zer0 <karizma@tutanota.com>
2020-09-02 08:17:02 +0200

    Render checkout page (view)

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -9,7 +9,7 @@ def store(request):
 	return render(request, 'store/store.html', context)
 
 def cart(request):
-	# query the order items
+	# query the order object and order items list
 	if request.user.is_authenticated:
 		customer = request.user.customer
 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
@@ -26,5 +26,18 @@ def cart(request):
 	return render(request, 'store/cart.html', context)
 
 def checkout(request):
-	context = {}
+		# query the order object and order items list
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all() # django automatically generates a set for each table/model that contain a FK for Order table/model 
+	else:
+		# if the user is guest (not registered)
+		items = [] # leave it empty for now
+		order = {'getTotal': 0, 'getItemsNumber': 0} # for prevent an error if we are logged out now
+
+	context = {
+		'items': items,
+		'order': order
+	}
 	return render(request, 'store/checkout.html', context)
